e9115033f821ac2409f850cf121ae20c09265fb7
Costas Grodis <grodiscostas@hotmail.com>
2022-01-09 23:29:27 +0200

    Initial commit

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        bool PlatformExists(int platformId);
+        void CreatePlatform(Platform platform);
+
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
